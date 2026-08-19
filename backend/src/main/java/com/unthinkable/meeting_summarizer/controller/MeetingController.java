ad79b1ff8f2277920c6c72c110d235fe69d4a649
KhushiGupta14 <khushigupta061402@gmai.com>
2026-08-20 01:04:17 +0530

    feat: connect meeting controller to service

--- backend/src/main/java/com/unthinkable/meeting_summarizer/controller/MeetingController.java
+++ backend/src/main/java/com/unthinkable/meeting_summarizer/controller/MeetingController.java
@@ -1,15 +1,43 @@
 package com.unthinkable.meeting_summarizer.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.unthinkable.meeting_summarizer.entity.Meeting;
+import com.unthinkable.meeting_summarizer.service.MeetingService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/meetings")
 public class MeetingController {
 
+    private final MeetingService meetingService;
+
+    public MeetingController(MeetingService meetingService) {
+        this.meetingService = meetingService;
+    }
+
     @GetMapping("/test")
     public String test() {
         return "Meeting Summarizer backend is running!";
+    }
+
+    @GetMapping
+    public List<Meeting> getAllMeetings() {
+        return meetingService.getAllMeetings();
+    }
+
+    @GetMapping("/{id}")
+    public Meeting getMeetingById(@PathVariable Long id) {
+        return meetingService.getMeetingById(id);
+    }
+
+    @PostMapping
+    public Meeting createMeeting(@RequestBody Meeting meeting) {
+        return meetingService.saveMeeting(meeting);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMeeting(@PathVariable Long id) {
+        meetingService.deleteMeeting(id);
     }
 }
