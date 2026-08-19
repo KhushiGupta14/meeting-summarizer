ea1dd3a93b51e768fc6b4cf1fa552a5135878ff4
KhushiGupta14 <khushigupta061402@gmai.com>
2026-08-20 00:26:44 +0530

    feat: add Spring Boot backend

---
+++ backend/src/test/java/com/unthinkable/meeting_summarizer/MeetingSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unthinkable.meeting_summarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingSummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
